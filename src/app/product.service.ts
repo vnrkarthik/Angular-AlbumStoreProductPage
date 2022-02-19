b9c26ff701676dd06f9e95d0b865ec545dd986fc
Sundaram <Karthik.Sundaram@realogy.com>
2022-02-19 13:37:03 +0530

    Adjusted as per pluralsight expectation

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) {
-    
-  }
-
   private _albumUrl: string = "../assets/album.json";
 
+  constructor(private _http: Http) { }
+
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => {
-      return response.json();
-    })
+    return this._http.get(this._albumUrl).map((response) => 
+      response.json());
   }
 
 }
